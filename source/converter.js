27a2ea27510bc7a04159fa20623bf13ae167fb16
AstroX10 <danielfx016@gmail.com>
2024-10-18 16:13:49 +0100

    fix

--- source/converter.js
+++ source/converter.js
@@ -1,14 +1,22 @@
 const { command } = require('../lib');
 const { STICKER_PACK } = require('../config');
+
 command(
  {
   pattern: 'sticker ?(.*)',
   desc: 'Converts Image/Video to Sticker',
   type: 'converter',
  },
- async (message) => {
-  if (!message.reply_message?.image || !message.reply_message?.video) return message.reply('_Reply Image/Video_');
+ async (message, match) => {
+  const hasMedia = message.reply_message?.image || message.reply_message?.video;
+  if (!hasMedia) return message.reply('_Reply with an Image/Video_');
   const content = await message.download(message.reply_message.data);
-  return message.send(content.buffer, { type: 'sticker', author: STICKER_PACK.split(';')[0], packname: STICKER_PACK.split(';')[1] });
+  if (!content.buffer) return message.reply('_Failed to convert media to sticker_');
+
+  return message.send(content.buffer, {
+   type: 'sticker',
+   author: STICKER_PACK.split(';')[0],
+   packname: STICKER_PACK.split(';')[1],
+  });
  }
 );
