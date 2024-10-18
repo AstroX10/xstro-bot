c96dc1893ff4c2646890e677e1b253e06fa2529f
AstroX10 <danielfx016@gmail.com>
2024-10-18 16:18:15 +0100

    fix

--- source/converter.js
+++ source/converter.js
@@ -9,11 +9,27 @@ command(
  },
  async (message, match) => {
   const hasMedia = message.reply_message?.image || message.reply_message?.video;
-  if (!hasMedia) return message.reply('_Reply with an Image/Video_');
+  if (!hasMedia) return message.reply('_Reply an Image/Video_');
   const content = await message.download(message.reply_message.data);
-  if (!content.buffer) return message.reply('_Failed to convert media to sticker_');
-
   return message.send(content.buffer, {
+   type: 'sticker',
+   author: STICKER_PACK.split(';')[0],
+   packname: STICKER_PACK.split(';')[1],
+  });
+ }
+);
+
+command(
+ {
+  pattern: 'take ?(.*)',
+  desc: 'Saves Stickers to be Yours',
+  type: 'converter',
+ },
+ async (message, match) => {
+  const isStickerMedia = message.reply_message?.sticker;
+  if (!isStickerMedia) return message.reply('_Reply A Sticker!_');
+  const newSticker = await message.download(message.reply_message.data);
+  return message.send(newSticker.buffer, {
    type: 'sticker',
    author: STICKER_PACK.split(';')[0],
    packname: STICKER_PACK.split(';')[1],
